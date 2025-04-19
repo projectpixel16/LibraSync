3bb6b73da6c13bb59bda3d80fae490f9a16a89d7
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2025-04-20 03:22:27 +0800

    pass

--- students/top_nav.php
+++ students/top_nav.php
@@ -50,7 +50,7 @@
             <div class="nav_menu">
                 <nav class="" role="navigation">
                     <div class="nav toggle">
-                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                        <a id="menu_toggle1"><i class="fa fa-bars"></i></a>
                     </div>
 
                     <ul class="nav navbar-nav navbar-right">
